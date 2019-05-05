c1e9eb1affb3ea6a79e916c798d3bfa3a95b6dc1
Chase Fleming <chaseefleming@gmail.com>
2019-05-05 15:45:20 -0700

    Dot notation

---
+++ src/Menu.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import style from './style.css';
+
+const MenuItem = (props) => {
+  const isActive = props.isActive === 'true';
+  const overrideStyle = props.options && props.options.override ? props.options.override : null;
+  const overrideActiveStyle = props.options && props.options.activeOverride ? props.options.activeOverride : null;
+  const classes = `${props.isActive === 'true' ? style.menuitemactive : style.menuitem}`;
+  const overrideToUse = isActive ? overrideActiveStyle : overrideStyle;
+  return <div className={classes} style={overrideToUse}>{props.name}</div>;
+}
+
+export class Menu extends Component {
+  static Item = MenuItem;
+
+  render() {
+    let classes = [];
+    
+    if (this.props.options && this.props.options.type) {
+      classes.push(style[this.props.options.type])
+    } else {
+      classes.push(style.menu)
+    }
+
+    const childrenWithProps = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, { options: this.props.options })
+    );
+
+    return (
+      <div className={classes.join(' ')}>
+        {childrenWithProps}
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,37 +1,3 @@
-import React, { Component } from 'react';
-import style from './style.css';
+import { Menu } from './Menu';
 
-export class Menu extends Component {
-  render() {
-    let classes = [];
-    
-    if (this.props.options && this.props.options.type) {
-      classes.push(style[this.props.options.type])
-    } else {
-      classes.push(style.menu)
-    }
-
-    const childrenWithProps = React.Children.map(this.props.children, child =>
-      React.cloneElement(child, { options: this.props.options })
-    );
-
-    return (
-      <div className={classes.join(' ')}>
-        {childrenWithProps}
-      </div>
-    );
-  }
-}
-
-export class MenuItem extends Component {
-  render() {
-    const isActive = this.props.isActive === 'true';
-    const overrideStyle = this.props.options && this.props.options.override ? this.props.options.override : null;
-    const overrideActiveStyle = this.props.options && this.props.options.activeOverride ? this.props.options.activeOverride : null;
-    const classes = `${this.props.isActive === 'true' ? style.menuitemactive : style.menuitem}`;
-    const overrideToUse = isActive ? overrideActiveStyle : overrideStyle;
-    return (
-      <div className={classes} style={overrideToUse}>{this.props.name}</div>
-    );
-  }
-}
+export default { Menu };
