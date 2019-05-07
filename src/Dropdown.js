401bec5eacd88e58cee5332a3477d55bde157fc3
Chase Fleming <chaseefleming@gmail.com>
2019-05-06 17:59:35 -0700

    dropdown menu / item

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import style from './style.css';
 
+
+const DropdownMenu = (props) => {
+  const childrenWithProps = React.Children.map(props.children, child =>
+    React.cloneElement(child, { options: props.options })
+  );
+  return <div className="dropdown-menu">{childrenWithProps}</div>;
+};
+
+const DropdownItem = (props) => {
+  return <div>{props.text}</div>;
+};
+
 export class Dropdown extends Component {
   constructor(props) {
     super(props);
@@ -8,16 +20,23 @@ export class Dropdown extends Component {
     this.toggleOpen = this.toggleOpen.bind(this);
   }
 
+  static Menu = DropdownMenu;
+  static Item = DropdownItem;
+
   toggleOpen() {
     this.setState({ isOpen: !this.state.isOpen });
   }
 
   render () {
+    const childrenWithProps = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, { options: this.props.options })
+    );
+
     return (
       <div className={style.dropdown} onClick={() => this.toggleOpen()}>
         <span>{this.props.text}</span>
         <div className={this.state.isOpen ? style.dropdownContentOpen : style.dropdownContent}>
-          <p>Dropdown Content</p>
+          {childrenWithProps}
         </div>
       </div>
     );
