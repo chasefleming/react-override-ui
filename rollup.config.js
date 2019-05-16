8898da583542a12192330396ae58b3e086380431
Chase Fleming <chaseefleming@gmail.com>
2019-05-16 16:36:31 -0700

    works

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import babel from 'rollup-plugin-babel';
-// import minify from 'rollup-plugin-babel-minify';
 import postcss from 'rollup-plugin-postcss';
-
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external'
